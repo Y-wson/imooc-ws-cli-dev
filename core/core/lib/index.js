3a5dde11d9df46ccab6d9f5922af65bb455d7e91
悦者生存 <1002783067@qq.com>
2022-11-14 20:24:42 +0800

    fix: add import local npmlog

---
+++ core/core/lib/index.js
@@ -0,0 +1,15 @@
+/*
+ * @Author: 悦者生存 1002783067@qq.com
+ * @Date: 2022-11-12 16:12:52
+ * @LastEditors: 悦者生存 1002783067@qq.com
+ * @LastEditTime: 2022-11-13 19:08:29
+ * @FilePath: /imooc-ws-cli-dev/core/core/lib/index.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+'use strict';
+
+module.exports = core;
+
+function core() {
+    console.log('exec core');
+}
