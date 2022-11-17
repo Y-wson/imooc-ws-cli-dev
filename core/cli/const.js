cd9b91ea1c795dbb70b8e0af97fe65405436a482
悦者生存 <1002783067@qq.com>
2022-11-17 22:54:12 +0800

    fix: edit checkPkgVersion method

--- core/cli/const.js
+++ core/cli/const.js
@@ -2,10 +2,10 @@
  * @Author: 悦者生存 1002783067@qq.com
  * @Date: 2022-11-17 22:38:51
  * @LastEditors: 悦者生存 1002783067@qq.com
- * @LastEditTime: 2022-11-17 22:49:03
+ * @LastEditTime: 2022-11-17 22:53:45
  * @FilePath: /imooc-ws-cli-dev/core/cli/const.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 module.exports = {
-  LOWEST_PKG_VERSION: "1.0.2",
+  LOWEST_PKG_VERSION: "1.0.0",
 };
--- core/cli/lib/index.js
+++ core/cli/lib/index.js
@@ -2,7 +2,7 @@
  * @Author: 悦者生存 1002783067@qq.com
  * @Date: 2022-11-12 16:12:52
  * @LastEditors: 悦者生存 1002783067@qq.com
- * @LastEditTime: 2022-11-17 22:51:57
+ * @LastEditTime: 2022-11-17 22:53:37
  * @FilePath: /imooc-ws-cli-dev/core/core/lib/index.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -23,6 +23,7 @@ function core() {
    
 }
 
+// 检测package.json版本是否大于最低版本
 function checkPkgVersion() {
     const currentPkgVersion = pkg.version;
     if (!semver.gte(currentPkgVersion, LOWEST_PKG_VERSION)) {
