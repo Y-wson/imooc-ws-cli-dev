0205175164271985042b52a690f93381d58f28e9
悦者生存 <1002783067@qq.com>
2022-11-19 13:52:17 +0800

    fix: add checkRoot and checkUserHome

--- core/cli/lib/index.js
+++ core/cli/lib/index.js
@@ -2,21 +2,25 @@
  * @Author: 悦者生存 1002783067@qq.com
  * @Date: 2022-11-12 16:12:52
  * @LastEditors: 悦者生存 1002783067@qq.com
- * @LastEditTime: 2022-11-17 22:53:37
+ * @LastEditTime: 2022-11-19 13:51:26
  * @FilePath: /imooc-ws-cli-dev/core/core/lib/index.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 'use strict';
 
 const semver = require('semver');
+const fs = require('fs-extra');
+const chalk = require('chalk');
 const log = require('@imooc-ws-cli-dev/log');
 const pkg = require('../../../package.json');
-const { LOWEST_PKG_VERSION } = require('../const');
+const { LOWEST_PKG_VERSION } = require('./const');
 
 
 function core() {
     try {
-      checkPkgVersion();
+        checkPkgVersion();
+        checkRoot();
+        checkUserHome();
     } catch (error) {
         log.warn(error);
     }
@@ -32,6 +36,20 @@ function checkPkgVersion() {
         )
     } else {
         log.info('cli', currentPkgVersion);
+    }
+}
+
+// 避免脚手架开发者使用root权限进行文件操作，导致其他协作开发者无法对文件进行修改
+function checkRoot() {
+    const rootCheck = require("root-check");
+    rootCheck();
+}
+
+// 检查用户主目录
+function checkUserHome() {
+    const userHome = require('user-home');
+    if (!userHome || fs.pathExistsSync(userHome)) {
+        throw new Error(chalk.red(`the user main directory is not exist`));
     }
 }
 
